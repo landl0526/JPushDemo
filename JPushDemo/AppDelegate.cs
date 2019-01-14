137edcc589b16cc3764ad6c55a1a761ce4388f5c
Land Lu <v-luting@microsoft.com>
2019-01-14 12:26:39 +0800

    Add project files.

---
+++ JPushDemo/AppDelegate.cs
@@ -0,0 +1,89 @@
+﻿using Foundation;
+using JPush.Binding.iOS;
+using System;
+using UIKit;
+using UserNotifications;
+
+namespace JPushDemo
+{
+	// The UIApplicationDelegate for the application. This class is responsible for launching the
+	// User Interface of the application, as well as listening (and optionally responding) to application events from iOS.
+	[Register("AppDelegate")]
+	public class AppDelegate : UIApplicationDelegate, IUNUserNotificationCenterDelegate
+	{
+		// class-level declarations
+
+		public override UIWindow Window
+		{
+			get;
+			set;
+		}
+
+		public override bool FinishedLaunching(UIApplication application, NSDictionary launchOptions)
+		{
+            // Override point for customization after application launch.
+            // If not required for your application you can safely delete this method
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                var authOptions = UNAuthorizationOptions.Alert | UNAuthorizationOptions.Badge | UNAuthorizationOptions.Sound;
+                UNUserNotificationCenter.Current.RequestAuthorization(authOptions, (granted, error) => {
+                    Console.WriteLine(granted);
+                });
+            }
+            else
+            {
+                var allNotificationTypes = UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound;
+                var settings = UIUserNotificationSettings.GetSettingsForTypes(allNotificationTypes, null);
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(settings);
+            }
+
+            UIApplication.SharedApplication.RegisterForRemoteNotifications();
+
+            UNUserNotificationCenter.Current.Delegate = this;
+
+            // JPush configuration
+            if (launchOptions == null)
+            {
+                launchOptions = new NSDictionary();
+            }
+            JPUSHService.SetupWithOption(launchOptions, "69d8c7f4f57fa26830226882", "Publish channel", false);
+            JPUSHService.SetDebugMode();
+
+            return true;
+		}
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            Console.WriteLine("Registered for remote notifications");
+            Console.WriteLine(deviceToken.GetBase64EncodedString(NSDataBase64EncodingOptions.None));
+            Console.WriteLine(deviceToken);
+            JPUSHService.RegisterDeviceToken(deviceToken);
+        }
+
+        public override void DidReceiveRemoteNotification(UIApplication application, NSDictionary userInfo, Action<UIBackgroundFetchResult> completionHandler)
+        {
+            Console.WriteLine("Received a notficaiton");
+
+            //ViewController viewController = Window.RootViewController as ViewController;
+            //viewController.location.RequestLocation();
+            completionHandler(UIBackgroundFetchResult.NewData);
+        }
+
+        [Export("userNotificationCenter:willPresentNotification:withCompletionHandler:")]
+        public void WillPresentNotification(UNUserNotificationCenter center, UNNotification notification, Action<UNNotificationPresentationOptions> completionHandler)
+        {
+            Console.WriteLine("Handling iOS 11 foreground notification");
+            completionHandler(UNNotificationPresentationOptions.Sound | UNNotificationPresentationOptions.Alert);
+        }
+
+        [Export("userNotificationCenter:didReceiveNotificationResponse:withCompletionHandler:")]
+        public void DidReceiveNotificationResponse(UNUserNotificationCenter center, UNNotificationResponse response, Action completionHandler)
+        {
+            var userInfo = response.Notification.Request.Content.UserInfo;
+            completionHandler();
+        }
+
+    }
+}
+
---
+++ JPushDemo/ViewController.cs
@@ -0,0 +1,65 @@
+﻿using CoreLocation;
+using Foundation;
+using System;
+using System.Linq;
+using UIKit;
+
+namespace JPushDemo
+{
+    public partial class ViewController : UIViewController
+    {
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+
+        public CLLocationManager location;
+        public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            location = new CLLocationManager();
+            location.RequestAlwaysAuthorization();
+            //location.StartUpdatingLocation();
+            location.PausesLocationUpdatesAutomatically = false;
+            location.AllowsBackgroundLocationUpdates = true;
+            location.DesiredAccuracy = CLLocation.AccuracyHundredMeters;
+            location.Delegate = new MyLocationDelegate(location);
+        }
+
+		public override void DidReceiveMemoryWarning ()
+		{
+			base.DidReceiveMemoryWarning ();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+        partial void LocationBtnClick(UIKit.UIButton sender)
+        {
+            location.RequestLocation();
+        }
+    }
+    public class MyLocationDelegate : CLLocationManagerDelegate
+    {
+        CLLocationManager locationManager;
+        public MyLocationDelegate(CLLocationManager locationManager)
+        {
+            this.locationManager = locationManager;
+        }
+        public override void LocationsUpdated(CLLocationManager manager, CLLocation[] locations)
+        {
+            CLLocation location = locations.FirstOrDefault();
+
+            var latitude = location.Coordinate.Latitude;
+            var longitude = location.Coordinate.Longitude;
+            Console.WriteLine("-----------" + latitude + "Longitude" + longitude);
+
+            //locationManager.StopUpdatingLocation();
+        }
+
+        public override void Failed(CLLocationManager manager, NSError error)
+        {
+            Console.WriteLine(error);
+        }
+    }
+}
